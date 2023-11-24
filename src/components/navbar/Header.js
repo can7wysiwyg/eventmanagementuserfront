105d88024d96384a1899a9bbb495b08baff0c821
can7wysiwyg <paulkssa@gmail.com>
2023-11-24 14:17:45 +0200

    updates

--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -1,12 +1,11 @@
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { GlobalState } from "../../GlobalState";
 import {  useContext } from "react";
-import { Link } from "react-router-dom";
  import 'bootstrap/dist/css/bootstrap.min.css';
+ import Logo from "../images/logo.png"
 
 const Header = () => {
   const state = useContext(GlobalState);
-  const [categories] = state.CategoryApi.categories
+  // const [categories] = state.CategoryApi.categories
   const [isLogged] = state.userApi.isLogged;
   const [isUser] = state.userApi.isUser;
 
@@ -17,41 +16,21 @@ const Header = () => {
     window.location.href = "/";
   };
 
-  function Webname() {
-    if (isLogged !== true) {
-      return (
-        <Link className="navbar-brand" to="/">
-          Events Marketer
-        </Link>
-      );
-    } else {
-      return (
-        <Link className="navbar-brand" to="/">
-          User
-        </Link>
-      );
-    }
-  }
-
-  const loggedRouter = () => {
-    return (
-      <Nav>
-        <Link className="nav-link" to="/" onClick={logoutUser}>
-          Logout
-        </Link>
-      </Nav>
-    );
-  };
-
+  
+ 
   const UserRoute = () => {
     return (
-      <Nav>
-        <NavDropdown title="Management" id="basic-nav-dropdown">
-          <NavDropdown.Item href="/management">My Panel</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
+      
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/management">My Panel</a>
+        </li>
+
+      
+      
     );
   };
+
+
 
   const UserLogin = () => {
     if (isUser) {
@@ -59,31 +38,57 @@ const Header = () => {
     }
   };
 
+  const loggedRouter = () => {
+    return (
+    
+        <li className="nav-item">
+        <a className="nav-link"href="/" onClick={logoutUser}>
+          Logout
+        </a>
+        </li>
+    
+    );
+  };
+
+
   return (
 
     <>
     <nav className="navbar navbar-expand-md navbar-dark" style={{backgroundColor: "#595B83" }}>
   <div className="container-fluid">
     <a className="navbar-brand" href="/">
-      <img src="https://codingyaar.com/wp-content/uploads/logo-1.png" alt="logo" />
+      <img src={Logo} alt="logo" />
     </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
+
       <ul className="navbar-nav mx-auto">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Features</a>
+          <a className="nav-link" href="/events">Trending Events</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Pricing</a>
+          <a className="nav-link" href="/new_events">New Events</a>
         </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/browse_events">Browse Event Types</a>
+        </li>
+        
+        
+
+        
+{isLogged ? loggedRouter() :  <li className="nav-item">  <a href="/login" className="nav-link" >Login</a> </li> }
+          {UserLogin()}
+
+
         <li className="nav-item">
           <a className="nav-link" href="/">Contact</a>
         </li>
+
       </ul>
     </div>
   </div>
